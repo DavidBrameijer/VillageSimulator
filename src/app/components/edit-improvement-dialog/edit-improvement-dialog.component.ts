7ec47defead68cad440699c520e8f338d8e7361e
imperfectsworld <imperfectsworld@gmail.com>
2024-09-16 10:22:25 -0400

    Added The option to add or upgrade an improvement must be disabled if there aren't enough resources to meet the costs. The option to downgrade or remove an improvement should be disabled if the provided resources would go below zero if that action were taken.

--- src/app/components/edit-improvement-dialog/edit-improvement-dialog.component.ts
+++ src/app/components/edit-improvement-dialog/edit-improvement-dialog.component.ts
@@ -1,37 +1,99 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Type } from '@angular/core';
 import { Improvement } from '../../models/improvement';
 import { VillageService } from '../../services/village.service';
 import { DataService } from '../../services/data.service';
+import { concatWith } from 'rxjs';
 
 @Component({
-  selector: 'app-edit-improvement-dialog',
-  standalone: true,
-  imports: [],
-  templateUrl: './edit-improvement-dialog.component.html',
-  styleUrl: './edit-improvement-dialog.component.css'
+	selector: 'app-edit-improvement-dialog',
+	standalone: true,
+	imports: [],
+	templateUrl: './edit-improvement-dialog.component.html',
+	styleUrl: './edit-improvement-dialog.component.css'
 })
 export class EditImprovementDialogComponent {
-	constructor(private village:VillageService, private data:DataService) {}
+	constructor(private village: VillageService, private data: DataService) { }
 
 	@Input() index: number = -1;
 
-	getImprovement() : Improvement {
+	getImprovement(): Improvement {
 		return this.village.improvements[this.index];
 	}
 
-	doUpgradeDowngrade(isUpgrade: boolean) : void {
+	doUpgradeDowngrade(isUpgrade: boolean): void {
 		if (isUpgrade) {
+			let requirement = this.data.getImprovementByName(this.getImprovement().type);
 			this.village.upgradeImprovement(this.index);
+			this.village.removeResources(requirement.cost);
+			this.village.addResources(requirement.product);
 		}
 		else {
+			let requirement = this.data.getImprovementByName(this.getImprovement().type);
 			this.village.downgradeImprovement(this.index);
+			this.village.addResources(requirement.cost);
+			this.village.removeResources(requirement.product);
 		}
 	}
 
-	destroyImprovement() : void {
+	destroyImprovement(): void {
+		let level = this.getImprovement().level;
 		let requirement = this.data.getImprovementByName(this.getImprovement().type);
 		this.village.removeImprovement(this.index);
-		this.village.addResources(requirement.cost);
-		this.village.removeResources(requirement.product);
+
+		for (let i = 0; i < level; i++) {
+			this.village.addResources(requirement.cost);
+			this.village.removeResources(requirement.product);
+		}
+
+
+	}
+
+	canUpgrade(): Boolean {
+		let type = this.getImprovement().type;
+		let upgrade = this.data.getImprovementByName(type)
+
+
+
+		for (const validUpgrade in upgrade.cost) {
+			if (this.village.resources.get(validUpgrade)! < (upgrade.cost as any)[validUpgrade]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	canDowngrade(): Boolean {
+
+		let type = this.getImprovement().type;
+		let upgrade = this.data.getImprovementByName(type)
+		if (this.getImprovement().level == 1) {
+			return false;
+		}
+
+		for (const validUpgrade in upgrade.product) {
+			if (this.village.resources.get(validUpgrade)! < (upgrade.product as any)[validUpgrade]) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+	canDestroy(): Boolean {
+
+		let type = this.getImprovement().type;
+		let upgrade = this.data.getImprovementByName(type)
+		let level = this.getImprovement().level;
+
+		for (const validUpgrade in upgrade.product) {
+
+			if (this.village.resources.get(validUpgrade)! < (upgrade.product as any)[validUpgrade] * level) {
+				return false;
+			}
+
+		}
+
+		return true;
+
 	}
 }
